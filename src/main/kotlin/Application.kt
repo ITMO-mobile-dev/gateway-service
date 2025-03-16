d81eae36f45311e9ba1356becde6b6b2dd2870ee
sh1kaMax <Shikmax2004@mail.ru>
2025-03-16 15:27:07 +0300

    task #2

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,5 +1,8 @@
 package ru.ifmo
 
+import io.ktor.client.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
 import kotlinx.serialization.Serializable
@@ -9,6 +12,12 @@ import kotlinx.serialization.Serializable
 * */
 @Serializable
 data class Response(val msg: String)
+
+val client = HttpClient {
+    install(ContentNegotiation) {
+        json()
+    }
+}
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
